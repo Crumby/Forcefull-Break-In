a906ab0ec25728ba070891a9595bbff57654078a
Standa Spacek <324802@mail.muni.cz>
2014-11-14 22:04:29 +0100

    lvl2 teren

---
+++ Assets/Scripts/StandardShot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StandardShot : MonoBehaviour {
+
+	public Transform target;
+	public float speed = 3f;
+	private bool ifTarget = false;
+	public void SetTarget(Transform helTarg){
+		target = helTarg;
+		ifTarget = true;
+	}
+	void Update () {
+		if(ifTarget){
+			Vector3 toTarget = target.position - transform.position;
+			rigidbody.velocity = toTarget.normalized * speed;
+			transform.forward = toTarget.normalized;
+		}
+	}
+}
---
+++ Assets/Scripts/TrackTarget.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackTarget : MonoBehaviour {
+
+	public Transform target;
+
+	public GameObject projectile;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire = 0.0f;
+
+	public float range;
+
+	void Update () {
+		transform.LookAt (target);
+
+		// fire only if target in range
+		float distance = Vector3.Distance (transform.position, target.transform.position);
+		if(distance <= range){
+			// continuous fire
+			if (Time.time > nextFire) {
+				nextFire = Time.time + fireRate;
+				Instantiate (projectile, shotSpawn.position, shotSpawn.rotation);
+				audio.Play ();
+			}
+		}
+	}
+}
